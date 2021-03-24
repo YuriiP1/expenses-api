550f35c620b43ddfc1ae90edcec66b8057139a64
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-03-24 16:37:23 +0200

    add transactional annotation

--- src/main/java/com/example/expensesapi/repository/ExpensesRepository.java
+++ src/main/java/com/example/expensesapi/repository/ExpensesRepository.java
@@ -3,9 +3,11 @@ package com.example.expensesapi.repository;
 import com.example.expensesapi.model.Expenses;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
 public interface ExpensesRepository extends JpaRepository<Expenses, Long> {
+    @Transactional
     List<Expenses> deleteByDate(Date date);
 }
