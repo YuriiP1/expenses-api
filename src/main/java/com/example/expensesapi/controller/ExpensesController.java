54f5a709cf7d585c9cff1db342aa6b24b8dfd3b9
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-03-24 15:17:10 +0200

    replace showAll() method

--- src/main/java/com/example/expensesapi/controller/ExpensesController.java
+++ src/main/java/com/example/expensesapi/controller/ExpensesController.java
@@ -1,14 +1,13 @@
 package com.example.expensesapi.controller;
 
 import com.example.expensesapi.model.Expenses;
-import com.example.expensesapi.model.Result;
 import com.example.expensesapi.service.ExpensesService;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class ExpensesController {
@@ -21,29 +20,29 @@ public class ExpensesController {
         this.restTemplate = restTemplate;
     }
 
+
     @GetMapping("/expenses")
-    public List<Expenses> showAll() {
-        List<Expenses> expenses = expensesService.findAll();
+    public Map<Date, List<Expenses>> showAll() {
 
-        Collections.sort(expenses, (e1, e2) ->
-                Long.valueOf(e1.getDate().getTime())
-                        .compareTo(e2.getDate().getTime())
-        );
+        List<Expenses> list = expensesService.findAll();
+        Map<Date, List<Expenses>> map = list
+                .stream()
+                .collect(Collectors.groupingBy(Expenses::getDate));
 
-        return expenses;
+        return map;
     }
 
     @PostMapping("/expenses")
     public Expenses save(@RequestBody Expenses expenses) {
-
+        expenses.setId(expensesService.count() + 1);
         return expensesService.save(expenses);
     }
 
     @DeleteMapping("/expenses")
-    public ResponseEntity<String> deleteByDate(@RequestParam("date")
+    public List<Expenses> deleteByDate(@RequestParam("date")
                                                    @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
-        expensesService.deleteByDate(date);
-        return ResponseEntity.ok("DELETED");
+        List<Expenses> deleteExpenses = expensesService.deleteByDate(date);
+        return deleteExpenses;
     }
 
     @GetMapping("/total")
