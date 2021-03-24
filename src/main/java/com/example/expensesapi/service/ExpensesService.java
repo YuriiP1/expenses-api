2168fb76647f5cd5bed33e51bd452699c28048ca
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-03-24 16:37:16 +0200

    remove transactional annotation

--- src/main/java/com/example/expensesapi/service/ExpensesService.java
+++ src/main/java/com/example/expensesapi/service/ExpensesService.java
@@ -29,7 +29,7 @@ public class ExpensesService {
         return expensesRepository.save(expenses);
     }
 
-    @Transactional
+
     public List<Expenses> deleteByDate(Date date) {
         return expensesRepository.deleteByDate(date);
     }
